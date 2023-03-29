01b43b9af3e178367c5517a7af332345fcdaa5d5
Jean BORT <jean.bort@emvista.com>
2023-03-29 17:04:16 +0200

    Fix

---
+++ src/test/java/starter/virtualModels/PetApiActions.java
@@ -0,0 +1,46 @@
+package starter.virtualModels;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static net.serenitybdd.rest.SerenityRest.then;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
+import net.serenitybdd.core.steps.UIInteractions;
+import org.hamcrest.Matchers;
+
+public class PetApiActions extends UIInteractions {
+
+  @Given("Kitty is available in the pet store")
+  public Long givenKittyIsAvailableInPetStore() {
+
+    Pet pet = new Pet("Kitty", "available");
+
+    Long newId = given()
+        .baseUri("https://petstore.swagger.io")
+        .basePath("/v2/pet")
+        .body(pet, ObjectMapperType.GSON)
+        .accept(ContentType.JSON)
+        .contentType(ContentType.JSON).post().getBody().as(Pet.class, ObjectMapperType.GSON).getId();
+    System.out.println("New id" + newId);
+    return newId;
+  }
+
+  @When("I ask for a pet using Kitty's ID: {0}")
+  public void whenIAskForAPetWithId(Long id) {
+    System.out.println("id = " + id);
+    Response response = when().get("https://petstore.swagger.io/v2/pet/" + id);
+    System.out.println("response = " + response);
+  }
+
+  @Then("I get Kitty as result")
+  public void thenISeeKittyAsResult() {
+    System.out.println("thenISeeKittyAsResult");
+    then().body("name", Matchers.equalTo("Kitty"));
+    System.out.println("ok");
+  }
+}
