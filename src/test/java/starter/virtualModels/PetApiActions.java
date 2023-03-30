3b9ea5bced4b74bd062aaaf21567bfcffed2689f
Jean BORT <jean.bort@emvista.com>
2023-03-30 07:33:34 +0200

    Fix test

--- src/test/java/starter/virtualModels/PetApiActions.java
+++ src/test/java/starter/virtualModels/PetApiActions.java
@@ -11,6 +11,7 @@ import io.restassured.http.ContentType;
 import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
 import net.serenitybdd.core.steps.UIInteractions;
+import net.serenitybdd.rest.SerenityRest;
 import org.hamcrest.Matchers;
 
 public class PetApiActions extends UIInteractions {
@@ -20,7 +21,7 @@ public class PetApiActions extends UIInteractions {
 
     Pet pet = new Pet("Kitty", "available");
 
-    Long newId = given()
+    Long newId = SerenityRest.given()
         .baseUri("https://petstore.swagger.io")
         .basePath("/v2/pet")
         .body(pet, ObjectMapperType.GSON)
@@ -33,14 +34,15 @@ public class PetApiActions extends UIInteractions {
   @When("I ask for a pet using Kitty's ID: {0}")
   public void whenIAskForAPetWithId(Long id) {
     System.out.println("id = " + id);
-    Response response = when().get("https://petstore.swagger.io/v2/pet/" + id);
+    Response response = SerenityRest.when().get("https://petstore.swagger.io/v2/pet/" + id);
     System.out.println("response = " + response);
   }
 
   @Then("I get Kitty as result")
   public void thenISeeKittyAsResult() {
     System.out.println("thenISeeKittyAsResult");
-    then().body("name", Matchers.equalTo("Kitty"));
+    SerenityRest.then().body("name", Matchers.equalTo("Kitty"));
     System.out.println("ok");
   }
+
 }
